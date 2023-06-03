43eac9190eee2c9bdde7baf2c170cbed0d264683
Someshfengde <someshfengde@gmail.com>
2023-06-03 13:58:29 +0530

    adding llama index tryout

---
+++ llama_index_tryout.py
@@ -0,0 +1,54 @@
+# %%
+import pandas as pd 
+import numpy as np 
+from pathlib import Path
+from glob import glob
+from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader, download_loader
+import os 
+# downloading the loader
+loader = download_loader("SimpleDirectoryReader")
+
+# the dataset files
+dataset_a1 = "dataset/IN-Ext/summary/full/A1"
+dataset_a2 = "dataset/IN-Ext/summary/full/A2"
+
+# loading the files 
+dataset_a1_files = glob(f"{dataset_a1}/*.txt")
+dataset_a2_files = glob(f"{dataset_a2}/*.txt")
+
+# combining the data
+comb_data = dataset_a1_files + dataset_a2_files
+
+# creating the documents
+documents = SimpleDirectoryReader(input_files=comb_data).load_data()
+# creating indexing from documents
+index = GPTVectorStoreIndex.from_documents(documents=documents, openai_api_key=os.environ["OPENAI_API_KEY"])
+
+# creating the query engine
+query_engine = index.as_query_engine()
+
+
+# getting response for query 
+
+response = query_engine.query("what is the law case?")
+print(response)
+
+def get_response(query):
+    # combining the data
+    comb_data = dataset_a1_files + dataset_a2_files
+
+    # creating the documents
+    documents = SimpleDirectoryReader(input_files=comb_data).load_data()
+    # creating indexing from documents
+    index = GPTVectorStoreIndex.from_documents(documents=documents, openai_api_key=os.environ["OPENAI_API_KEY"])
+
+    # creating the query engine
+    query_engine = index.as_query_engine()
+    response = query_engine.query(query)
+    return response
+
+
+# %%
+
+
+
