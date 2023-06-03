fa730cc35405eeb823f05d89a6784ac6a69e7e96
Someshfengde <someshfengde@gmail.com>
2023-06-03 23:51:43 +0530

    adding new cases text files

--- convert_pdf_text.py
+++ convert_pdf_text.py
@@ -3,8 +3,8 @@ from glob import glob
 from tqdm import tqdm 
 
 # Create the 'converted_text' directory if it doesn't exist
-if not os.path.exists("converted_text"):
-    os.makedirs("converted_text")
+if not os.path.exists("conv_text_new"):
+    os.makedirs("conv_text_new")
 
 filenames = glob("dataset/*.PDF")  # get document filenames
 for fname in tqdm(filenames):
@@ -12,7 +12,7 @@ for fname in tqdm(filenames):
         text = chr(12).join([page.get_text() for page in doc])
     
     # Create a new .txt file with the corresponding name and write the content of the PDF into it
-    txt_filename = "converted_text/" + os.path.basename(fname[:fname.find('.pdf')]) + ".txt"
+    txt_filename = "conv_text_new/" + os.path.basename(fname[:fname.find('.pdf')]) + ".txt"
 
     with open(txt_filename, "w", encoding="utf-8") as txt_file:
         txt_file.write(text)
